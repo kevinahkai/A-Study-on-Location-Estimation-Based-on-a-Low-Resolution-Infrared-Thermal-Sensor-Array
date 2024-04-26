ccc0b744adf45b6e1d9074c230c08f94ede2bb12
kevinahkai <kevin203700@gmail.com>
2024-04-26 16:03:23 +0900

    Version 4(Using FuncAnimation to update heatmap, looks more comfortable)

--- dynamic.py
+++ dynamic.py
@@ -1,9 +1,10 @@
-import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import numpy as np
 import busio
 import board
 import adafruit_amg88xx
-import matplotlib.pyplot as plt
-import numpy as np
+import time
 
 # I2Cバスの初期化
 i2c_bus = busio.I2C(board.SCL, board.SDA)
@@ -12,59 +13,44 @@ i2c_bus = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_amg88xx.AMG88XX(i2c_bus, addr=0x68)
 
 # センサーの初期化待ち
-time.sleep(.1)
+time.sleep(0.1)
 
-# 8x8ピクセルの画像とbicubic補間をした画像を並べて表示させる
-plt.subplots(figsize=(8, 4))
-
+fig, axes = plt.subplots(1, 2, figsize=(8, 4))
 slide_window = []
 window_size = 5
 
-# 设置更新间隔 ##新
-update_interval = 0.1  # 每0.1秒更新一次，以符合感测器的最大频率
-last_time = time.time()
+def heatmap(frame):
+    # データ取得
+    sensordata = np.array(sensor.pixels)
 
-# ループ開始
-while True:
-    current_time = time.time()
-    if current_time - last_time >= update_interval:
-        # データ取得
-        sensordata = np.array(sensor.pixels)
+    # Update queue
+    slide_window.append(sensordata)
+    if len(slide_window) > window_size:
+        slide_window.pop(0)
 
-        # Update queue
-        slide_window.append(sensordata)
-        if len(slide_window) > window_size:
-            slide_window.pop(0)
+    # 當前數據中最熱點的座標(人體位置)
+    y, x = np.unravel_index(np.argmax(sensordata), sensordata.shape)
 
-        # 當前數據中最熱點的座標(人體位置)
-        y, x = np.unravel_index(np.argmax(sensordata), sensordata.shape)
+    # 更新左側的圖表
+    axes[0].clear()
+    axes[0].imshow(sensordata, cmap="inferno", interpolation="bicubic")
+    axes[0].scatter(x, y, color='white', s=120)  # 標記最熱點
+    axes[0].set_title("Original Data")
 
-        # 显示原始数据
-        plt.subplot(1, 2, 1)
-        plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
-        plt.colorbar()
-        plt.scatter(x, y, color='white', s=120)  # 標記最熱點
-        plt.title("Original Data")
-        
-        # Calculate moving average if enough data is collected
-        if len(slide_window) == window_size:
-            ma = np.mean(slide_window, axis=0)
-            ma_y, ma_x = np.unravel_index(np.argmax(ma), ma.shape)
-            plt.subplot(1, 2, 2)
-            plt.imshow(ma, cmap="inferno", interpolation="bicubic")
-            plt.colorbar()
-            plt.scatter(ma_x, ma_y, color='white', s=120)  # 標記最熱點
-            plt.title("Moving Average")
-        else:
-            # 显示原始数据
-            plt.subplot(1, 2, 2)
-            plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
-            plt.colorbar()
-            plt.title("Original Data")
+    # Calculate moving average if enough data is collected
+    if len(slide_window) == window_size:
+        ma = np.mean(slide_window, axis=0)
+        ma_y, ma_x = np.unravel_index(np.argmax(ma), ma.shape)
+        axes[1].clear()
+        axes[1].imshow(ma, cmap="inferno", interpolation="bicubic")
+        axes[1].scatter(ma_x, ma_y, color='white', s=120)  # 標記最熱點
+        axes[1].set_title("Moving Average")
+    else:
+        axes[1].clear()
+        axes[1].imshow(sensordata, cmap="inferno", interpolation="bicubic")
+        axes[1].set_title("Original Data")
 
-        # 更新显示
-        plt.pause(0.1)
-        # 清除之前的图像
-        plt.clf()
-        
-        last_time = current_time
+# 使用 FuncAnimation 持續更新圖表
+ani = FuncAnimation(fig, heatmap, interval=100)  # 每100毫秒更新一次
+
+plt.show()
