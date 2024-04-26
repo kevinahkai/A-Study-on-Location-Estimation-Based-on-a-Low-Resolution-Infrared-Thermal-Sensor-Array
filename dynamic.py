f0396b35888657e7de4355fff4b86c5abc67b923
kevinahkai <kevin203700@gmail.com>
2024-04-26 18:28:13 +0900

    Version 6(Change to double slide_window(The moving_average of moving_average))

--- dynamic.py
+++ dynamic.py
@@ -16,8 +16,10 @@ sensor = adafruit_amg88xx.AMG88XX(i2c_bus, addr=0x68)
 time.sleep(0.1)
 
 fig, axes = plt.subplots(1, 2, figsize=(8, 4))
-slide_window = []
-window_size = 5
+first_slide_window = []
+second_slide_window = []
+window_size = 3
+second_window_size = 3
 
 # 初始化熱力圖和 colorbar
 sensordata = np.array(sensor.pixels)
@@ -28,13 +30,22 @@ for i in range(2):
     plt.colorbar(img, ax=axes[i])
 
 def heatmap(frame):
+    global first_slide_window, second_slide_window
     # データ取得
     sensordata = np.array(sensor.pixels)
 
-    # Update queue
-    slide_window.append(sensordata)
-    if len(slide_window) > window_size:
-        slide_window.pop(0)
+    # Update first level queue
+    first_slide_window.append(sensordata)
+    if len(first_slide_window) > window_size:
+        first_slide_window.pop(0)
+
+    # Calculate first level moving average if enough data is collected
+    if len(first_slide_window) == window_size:
+        first_ma = np.mean(first_slide_window, axis=0)
+        # Update second level queue
+        second_slide_window.append(first_ma)
+        if len(second_slide_window) > second_window_size:
+            second_slide_window.pop(0)
 
     # 當前數據中最熱點的座標(人體位置)
     y, x = np.unravel_index(np.argmax(sensordata), sensordata.shape)
@@ -49,11 +60,11 @@ def heatmap(frame):
                 transform=axes[0].transAxes, color='white', fontsize=10)
 
     # Calculate moving average if enough data is collected
-    if len(slide_window) == window_size:
-        ma = np.mean(slide_window, axis=0)
-        ma_y, ma_x = np.unravel_index(np.argmax(ma), ma.shape)
+    if len(second_slide_window) == second_window_size:
+        second_ma = np.mean(second_slide_window, axis=0)
+        ma_y, ma_x = np.unravel_index(np.argmax(second_ma), second_ma.shape)
         axes[1].clear()
-        axes[1].imshow(ma, cmap="inferno", interpolation="bicubic")
+        axes[1].imshow(second_ma, cmap="inferno", interpolation="bicubic")
         axes[1].scatter(ma_x, ma_y, color='white', s=120)  # 標記最熱點
         axes[1].set_title("Moving Average")
         axes[1].text(0.95, 0.95, f'Hotspot\nx: {ma_x}\ny: {ma_y}', 
