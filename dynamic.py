aa2db82fa406c9dd129c033a68e81fd428561ed0
kevinahkai <kevin203700@gmail.com>
2024-04-26 16:01:40 +0900

    Version 3(It's okay but a little delay)

--- dynamic.py
+++ dynamic.py
@@ -18,48 +18,53 @@ time.sleep(.1)
 plt.subplots(figsize=(8, 4))
 
 slide_window = []
+window_size = 5
+
+# 设置更新间隔 ##新
+update_interval = 0.1  # 每0.1秒更新一次，以符合感测器的最大频率
+last_time = time.time()
 
 # ループ開始
 while True:
-    # データ取得
-    #sensordata = sensor.pixels
-    sensordata = np.array(sensor.pixels)
+    current_time = time.time()
+    if current_time - last_time >= update_interval:
+        # データ取得
+        sensordata = np.array(sensor.pixels)
 
-    #Continue update queue
-    #這邊的視窗大小要重新設定，要慢慢實驗
-    slide_window.append(sensordata)
-    if len(slide_window) > 30:
-        slide_window.pop(0)
+        # Update queue
+        slide_window.append(sensordata)
+        if len(slide_window) > window_size:
+            slide_window.pop(0)
 
-    #Calculate moving average
-    ma = np.mean(slide_window, axis=0) if len(slide_window) == 3 else None
+        # 當前數據中最熱點的座標(人體位置)
+        y, x = np.unravel_index(np.argmax(sensordata), sensordata.shape)
 
-    # 當前數據中最熱點的座標(人體位置)
-    y, x = np.unravel_index(np.argmax(sensordata), sensordata.shape)
+        # 显示原始数据
+        plt.subplot(1, 2, 1)
+        plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
+        plt.colorbar()
+        plt.scatter(x, y, color='white', s=120)  # 標記最熱點
+        plt.title("Original Data")
+        
+        # Calculate moving average if enough data is collected
+        if len(slide_window) == window_size:
+            ma = np.mean(slide_window, axis=0)
+            ma_y, ma_x = np.unravel_index(np.argmax(ma), ma.shape)
+            plt.subplot(1, 2, 2)
+            plt.imshow(ma, cmap="inferno", interpolation="bicubic")
+            plt.colorbar()
+            plt.scatter(ma_x, ma_y, color='white', s=120)  # 標記最熱點
+            plt.title("Moving Average")
+        else:
+            # 显示原始数据
+            plt.subplot(1, 2, 2)
+            plt.imshow(sensordata, cmap="inferno", interpolation="bicubic")
+            plt.colorbar()
+            plt.title("Original Data")
 
-    # 移動平均值中的最熱點座標(人體位置)
-    ma_y, ma_x = np.unravel_index(np.argmax(ma), ma.shape)
-
-    # 8x8ピクセルのデータ
-    plt.subplot(1, 2, 1)
-    fig = plt.imshow(sensordata, cmap="inferno")
-    plt.colorbar()
-    plt.scatter(x, y, color='black', s=120)  # 標記最熱點
-    plt.title("Original Data")
-    # 顯示人體位置座標
-    #plt.text(x, y, f'({x}, {y})', color='black', ha='right', va='bottom')
-    plt.text(0.5, 0.5, f'Hotspot\nx: {x}\ny: {y}', horizontalalignment='center', verticalalignment='center')
-
-    #Location Detection
-    plt.subplot(1, 2, 2)
-    fig = plt.imshow(ma, cmap="inferno")
-    plt.colorbar()
-    plt.scatter(ma_x, ma_y, color='black', s=120)  # 在移动平均图中标记最热点
-    plt.title("Location Estimation")
-    # 顯示人體位置座標
-    plt.text(0.5, 0.5, f'Hotspot\nx: {ma_x}\ny: {ma_y}', horizontalalignment='center', verticalalignment='center')
-
-    # 如果使用plt.show，會停止，所以使用pause
-    # 如果不做plt.clf，會顯示很多彩條
-    plt.pause(.1)
-    plt.clf()
+        # 更新显示
+        plt.pause(0.1)
+        # 清除之前的图像
+        plt.clf()
+        
+        last_time = current_time
